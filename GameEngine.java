5503a84e5fd676ca2dccbb1f67d894152f1ff914
Pakamon11909 <pakamon_11909@hotmail.com>
2016-04-23 15:18:41 +0700

    #01
    ใส่พื้นหลัง และ สร้างกล่อง

---
+++ GameEngine.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+
+public class GameEngine {
+	GamePanel gp;
+	private SpaceShip v;	
+
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		gp.updateGameUI();
+		
+	}
+}
