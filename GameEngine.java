91914c3f1772f67163942536e818700e27bcd0c2
Pakamon11909 <pakamon_11909@hotmail.com>
2016-04-29 00:42:01 +0700

    #3 SpaceShip Movement and Add Enemy
    ปรับปรุงให้ส่วนของผู้เล่นสามารถเคลื่อนที่ซ้าย-ขวาได้
    กำหนดสีใหม่ให้กับพื้นหลังเกมและส่วนของผู้เล่น
    เพิ่มอุปสรรคในเกม และกำหนดลักษณะของอุปสรรคนั้น

--- GameEngine.java
+++ GameEngine.java
@@ -3,17 +3,88 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class GameEngine {
-	GamePanel gp;
-	private SpaceShip v;	
+public class GameEngine implements KeyListener{
 
-	public GameEngine(GamePanel gp, SpaceShip v) {
-		this.gp = gp;
-		this.v = v;		
-		
-		gp.sprites.add(v);
-		gp.updateGameUI();
-		
-	}
-}
+ 		GamePanel gp;
+ 		private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+ 		private SpaceShip v;	
+ 		private Timer time;
+ 
+ 		public GameEngine(GamePanel gp, SpaceShip v) {
+ 			this.gp = gp;
+ 			this.v = v;		
+ 		
+ 			gp.sprites.add(v);
+ 			
+ 			time = new Timer(50, new ActionListener() {
+ 			
+ 				@Override
+ 				public void actionPerformed(ActionEvent arg0) {
+ 					process();
+ 				}
+ 			});
+ 			time.setRepeats(true);
+ 
+ 		}
+ 
+ 		public void start(){
+ 			time.start();
+ 		}
+ 		private void generateEnemy(){
+ 			Enemy e = new Enemy((int)(Math.random()*390), 30);
+ 			gp.sprites.add(e);
+ 			enemies.add(e);
+ 		}
+ 
+ 		private void process(){
+
+ 			generateEnemy();
+ 
+ 			Iterator<Enemy> e_iter = enemies.iterator();
+ 			while(e_iter.hasNext()){
+				Enemy e = e_iter.next();
+ 				e.proceed();
+ 			
+ 				if(!e.isAlive()){
+ 					e_iter.remove();
+ 					gp.sprites.remove(e);
+ 				}
+ 			}
+ 			gp.updateGameUI();
+ 		}
+ 		public void die(){
+ 			time.stop();
+ 		}
+ 
+ 		void controlVehicle(KeyEvent e) {
+ 			switch (e.getKeyCode()) {
+ 			case KeyEvent.VK_LEFT:
+ 				v.move(-1);
+ 				break;
+ 			case KeyEvent.VK_RIGHT:
+ 				v.move(1);
+ 				break;
+ 			}
+ 		}
+ 	
+ 	@Override
+ 	public void keyPressed(KeyEvent e) {
+ 		controlVehicle(e);
+ 		
+ 	}
+ 
+ 	@Override
+ 	public void keyReleased(KeyEvent e) {
+ 		//do nothing
+ 	}
+ 
+ 	@Override
+ 	public void keyTyped(KeyEvent e) {
+ 		//do nothing		
+ 	}
+ 	
+ } 
--- Main.java
+++ Main.java
@@ -1,19 +1,32 @@
-import java.awt.BorderLayout;
-
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JFrame;
+import java.util.ArrayList;
+  
+  public class Main {
+ 
+  	public static void main(String[] args){
+  		JFrame frame = new JFrame("Space War");
+  		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  		frame.setSize(400, 650);
+  		frame.getContentPane().setLayout(new BorderLayout());
 
-public class Main {
-	public static void main(String[] args){
-		JFrame frame = new JFrame("Space War");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 650);
-		frame.getContentPane().setLayout(new BorderLayout());
-		
-		SpaceShip v = new SpaceShip(180, 550, 20, 20);
-		GamePanel gp = new GamePanel();
-		GameEngine engine = new GameEngine(gp, v);
+ 		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+ 		GamePanel gp = new GamePanel();
+ 		gp.sprites.add(v);
+  		gp.updateGameUI();
+
+ 		
+ 		GameEngine engine = new GameEngine(gp, v);
+ 		frame.addKeyListener(engine);
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
 
-	}
-}
+ 
+  		engine.start();
+ 	}
+  
+ } 
+ 	
